b5b2ba8f533da8cf5c1aedf4b7a828723cb2b339
RB Whitaker <RB.Whitaker@asirobots.com>
2020-05-08 10:49:19 -0600

    First pass.

---
+++ CSharpPlayersGuide.BatStuff/BatConsole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BatConsole
+{
+    public class BatConsole
+    {
+        public static void WriteLine(string message)
+        {
+            Console.WriteLine($"Holy {message}, Batman!");
+        }
+
+        public static void Write(string message)
+        {
+            Console.Write($"Holy {message}");
+        }
+    }
+}
